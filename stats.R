dbda8634816cef221b9cb047baf7c895ca98326e
AAA1exa8 <alexandrpleskot@gmail.com>
2022-11-03 11:32:41 +0100

    rework

--- stats.R
+++ stats.R
@@ -1,11 +1,12 @@
-mean_zaci_nema_psychologa <- mid_school %>%
-  select(pocet_zaku, psycholog) %>%
-  filter(psycholog == "ne")
+# vezmeme jen sloupce s počtem žáků a psycholog a z nich si vezmeme jen řákdy kde není psycholog
+mean_zaci_psycholog <- mid_school %>%
+  group_by(psycholog) %>%
+  summarise(avgZaku = mean(pocet_zaku))
+mean_zaci_specped <- mid_school %>%
+  group_by(spec_pedagog) %>%
+  summarise(avgZaku = mean(pocet_zaku))
 
-mean_zaci_nema_psychologa <- mean(mean_zaci_nema_psychologa$pocet_zaku)
 
-mean_zaci_nema_specped <- mid_school %>%
-  select(pocet_zaku, spec_pedagog) %>%
-  filter(spec_pedagog == "ne")
 
-mean_zaci_nema_specped <- mean(mean_zaci_nema_specped$pocet_zaku)
+
+
